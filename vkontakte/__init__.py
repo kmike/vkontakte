e52732379b757b6be3e09f313caef4b872998cd6
moriarty@yandex-team.ru <moriarty@yandex-team.ru>
2010-08-25 11:25:20 +0400

    added timeout for calls

--- vkontakte/__init__.py
+++ vkontakte/__init__.py
@@ -1,8 +1,11 @@
 # coding: utf-8
+from __future__ import with_statement
+from contextlib import closing
+
 import random
 import time
 import urllib
-import urllib2
+import httplib
 from hashlib import md5
 from functools import partial
 try:
@@ -11,7 +14,12 @@ except ImportError:
     import simplejson as json
 
 class VKError(Exception):
-    pass
+    __slots__ = ["code", "description", "params"]
+    def __init__(self, code, description, params):
+        self.code, self.description, self.params = (code, description, params)
+        Exception.__init__(self, str(self))
+    def __str__(self):
+        return "Error(code = '%s', description = '%s', params = '%s')" % (self.code, self.description, self.params)
 
 API_URL = 'http://api.vk.com/api.php'
 
@@ -20,7 +28,7 @@ def _sig(api_secret, **kwargs):
     params = "".join(["%s=%s" % (key, kwargs[key]) for key in keys])
     return md5(params+str(api_secret)).hexdigest()
 
-def request(api_id, api_secret, method, timestamp=None, **kwargs):
+def request(api_id, api_secret, method, timestamp=None, timeout = 1, **kwargs):
     params = dict(
         api_id = str(api_id),
         method = method,
@@ -32,7 +40,24 @@ def request(api_id, api_secret, method, timestamp=None, **kwargs):
     params.update(kwargs)
     params['sig'] = _sig(api_secret, **params)
     data = urllib.urlencode(params)
-    return urllib2.urlopen(API_URL, data).read()
+
+    host_port = API_URL.split('/')[2]
+    timeout_set = False
+    try:
+        connection = httplib.HTTPConnection(host_port, timeout = timeout)
+        timeout_set = True
+    except TypeError:
+        connection = httplib.HTTPConnection(host_port)
+
+    with closing(connection):
+        if not timeout_set:
+            connection.connect()
+            connection.sock.settimeout(timeout)
+            timeout_set = True
+
+        connection.request("POST", API_URL, data, {"Accept": "application/json", "Content-Type": "application/x-www-form-urlencoded"})
+        response = connection.getresponse()
+        return (response.status, response.read())
 
 
 class API(object):
@@ -41,12 +66,14 @@ class API(object):
         self.api_secret = api_secret
         self.defaults = defaults
 
-    def get(self, method, **kwargs):
-        assert kwargs.get('format', 'JSON') == 'JSON', 'Only JSON in supported'
-        response = request(self.api_id, self.api_secret, method, **kwargs)
+    def get(self, method, timeout = 1, **kwargs):
+        status, response = request(self.api_id, self.api_secret, method, timeout = timeout, **kwargs)
+        if not (status >= 200 and status <= 299):
+            raise VKError(status, "HTTP error", kwargs)
+
         data = json.loads(response)
-        if 'error' in data:
-            raise VKError(data['error'])
+        if "error" in data:
+            raise VKError(data["error"]["error_code"], data["error"]["error_msg"], data["error"]["request_params"])
         return data['response']
 
     # some magic to convert instance attributes into method names
