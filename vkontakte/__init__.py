b91c140af6600202b1787d1e2e8fe7fed0fcd51f
Mike Korobov <kmike84@gmail.com>
2010-08-25 14:57:10 +0600

    Low-level code is moved to it's own module

--- vkontakte/__init__.py
+++ vkontakte/__init__.py
@@ -1,17 +1,16 @@
 # coding: utf-8
-from __future__ import with_statement
-from contextlib import closing
-
 import random
 import time
 import urllib
-import httplib
 from hashlib import md5
 from functools import partial
 try:
     import json
 except ImportError:
     import simplejson as json
+from vkontakte import http
+
+API_URL = 'http://api.vk.com/api.php'
 
 class VKError(Exception):
     __slots__ = ["code", "description", "params"]
@@ -20,8 +19,6 @@ class VKError(Exception):
         Exception.__init__(self, str(self))
     def __str__(self):
         return "Error(code = '%s', description = '%s', params = '%s')" % (self.code, self.description, self.params)
-
-API_URL = 'http://api.vk.com/api.php'
 
 def _sig(api_secret, **kwargs):
     keys = sorted(kwargs.keys())
@@ -41,23 +38,10 @@ def request(api_id, api_secret, method, timestamp=None, timeout = 1, **kwargs):
     params['sig'] = _sig(api_secret, **params)
     data = urllib.urlencode(params)
 
-    host_port = API_URL.split('/')[2]
-    timeout_set = False
-    try:
-        connection = httplib.HTTPConnection(host_port, timeout = timeout)
-        timeout_set = True
-    except TypeError:
-        connection = httplib.HTTPConnection(host_port)
-
-    with closing(connection):
-        if not timeout_set:
-            connection.connect()
-            connection.sock.settimeout(timeout)
-            timeout_set = True
-
-        connection.request("POST", API_URL, data, {"Accept": "application/json", "Content-Type": "application/x-www-form-urlencoded"})
-        response = connection.getresponse()
-        return (response.status, response.read())
+    # urllib2 doesn't support timeouts for python 2.5 so
+    # custom function is used for making http requests
+    headers = {"Accept": "application/json", "Content-Type": "application/x-www-form-urlencoded"}
+    return http.post(API_URL, data, headers, timeout)
 
 
 class API(object):
